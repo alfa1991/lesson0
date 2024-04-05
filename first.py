ee3b7123930bf23b6a8f6aab7951c2c724d58279
alfa1991 <72934972+alfa1991@users.noreply.github.com>
2024-04-05 13:57:11 +0500

    Add files via upload

---
+++ first.py
@@ -0,0 +1,15 @@
+print(type(23891471923807487.142352314353455)) #float плавающее значение
+print(type(5)) #integer целое число
+print(5 + 5)
+print(5 - 5)
+print(5 * 5)
+print(5 / 5) #деление
+print(5 // 5) #целочисленное деление
+print(125 % 2) #остаток от деления
+print(5 ** 5) #возведение в степень
+print(type("hello world")) #string - строка
+print(type(True), type(False)) #boolean
+print(5>10, 10>5) # > < == != неравенство объектов
+print(5!=10 and 5==5) #and должны быть истинны оба выражения
+print(5>10 and 5!=5)
+print(5>10 or 5==5) #or должен быть истина одно выражение
